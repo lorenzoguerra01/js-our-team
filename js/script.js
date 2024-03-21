cbabc60fdb70f5eb54535c63624d9460ed136b10
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-03-21 17:45:31 +0100

    add members array, add array on DOM

--- js/script.js
+++ js/script.js
@@ -12,3 +12,50 @@
 // Organizzare i singoli membri in card/schede
 // BONUS 3:
 // Permettere l'aggiunta di un nuovo membro del team
+
+const members = [
+    {
+        image: 'wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO'
+    },
+    {
+        image: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor'
+    },
+    {
+        image: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager'
+    },
+    {
+        image: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager'
+    },
+    {
+        image: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer'
+    },
+    {
+        image: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer'
+    },
+];
+
+const reply = document.getElementById("reply")
+
+for (let i = 0; i < members.length; i++) {
+    const card = document.createElement('div');
+    card.classList.add("card", "p-3", "text-center")
+    card.style.width = `calc(100% / 3})`;
+    card.innerHTML = `
+    <img src="img/${members[i].image}">
+    <div>${members[i].name}</div>
+    <div>${members[i].role}</div>
+    `;
+    reply.appendChild(card);
+}
